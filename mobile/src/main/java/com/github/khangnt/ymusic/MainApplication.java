47aabd4c653a4ed373793ab8d4e35da9a12d8cff
Khang Nguyen <khang.neon.1997@gmail.com>
2017-01-03 08:42:02 +0700

    Add Tray - process safe preference

---
+++ mobile/src/main/java/com/github/khangnt/ymusic/MainApplication.java
@@ -0,0 +1,11 @@
+package com.github.khangnt.ymusic;
+
+import android.app.Application;
+
+/**
+ * Created by Khang NT on 1/3/17.
+ * Email: khang.neon.1997@gmail.com
+ */
+public class MainApplication extends Application {
+    private static final String TAG = "MainApplication";
+}
